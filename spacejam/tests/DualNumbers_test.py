4452894b3f63a56a7844a276beffb1543603a06c
Lauren Yoo <jyoo@hsph.harvard.edu>
2018-11-07 22:16:57 -0500

    Passed PyTest

--- spacejam/tests/DualNumbers_test.py
+++ spacejam/tests/DualNumbers_test.py
@@ -1,47 +1,51 @@
 import pytest
-import spacejam.Dual as Dual
+import spacejam.Dual as sp
 import numpy as np 
 
-class TestComplex():
-	def test_dual(self):
-		z= Dual.Dual(1.0,2.0)
-		assert z.r=1.0
-		assert z.d=2.0
-    def test_add(self):
-        
-        z= Dual.Dual(1.0, 2.0) + Dual.Dual(3.0, 4.0)
-        assert z.r == 3.0
-        assert z.d == 6.0
 
-    def test_divide(self,other):
-        z = Dual.Dual(2.0, 4.0) 
-        result=z.trudiv(2.0)
-        assert z.r== 1.0
-        assert z.d== 2.0 
+def test_dual():
 
-    def test_multiply(self,other):
-    	x=Dual.Dual(2.0, 4.0) 
-    	y=Dual.Dual(1.0,1.0)
-    	z=x.mul(y)
-    	assert z.r==2.0
-    	assert z.d==6.0
+	z= sp.Dual(1.0,2.0)
+	assert z.r==1.0
+	assert z.d==2.0
+
+def test_add():
+	x=sp.Dual(1.0, 2.0); 
+	y=sp.Dual(3.0, 4.0)
+	z= x+y
+	assert z.r == 4.00
+	assert z.d == 6.00
+
+def test_divide():
+	# real = (self.r*other)/other**2
+ #    dual = (self.d*other)/other**2
+	z = sp.Dual(1.0, 4.0) 
+	result=z/2.0
+	assert result.r==0.5
+	assert result.d==2.00
 
 
-    def test_sin(self):
-    	x=Dual.Dual(3.0, 4.0)
-        z = x.sin()
-        assert z.r==np.sin(3.0)
-        assert z.d==4*np.cos(3.0)
+def test_multiply():
+	x=sp.Dual(2.0, 4.0) 
+	y=sp.Dual(1.0,1.0)
+	z=x * y 	 
+	assert z.r==2.00
+	assert z.d==6.00
 
-    def test_cos(self):
-    	x=Dual.Dual(3.0, 4.0)
-    	z=x.cos()
-    	assert z.r==np.cos(3.0)
-    	assert z.d==-4.0*np.sin(3.0)
 
-    def test_tan(self):
-    	x=Dual.Dual(3.0, 4.0)
-    	z= x.sin()/x.cos()
-    	assert x.tan()==Dual.Dual(z.r, z.d)
+def test_sin():
+	x=sp.Dual(3.0, 4.0)
+	z = x.sin()
+	assert z.r==np.sin(3.0)
+	assert z.d==4*np.cos(3.0)
+
+def test_cos():
+	x=sp.Dual(3.0, 4.0)
+	z=x.cos()
+	assert z.r==np.cos(3.0)
+	assert z.d==-4.0*np.sin(3.0)
+
+	
+
 
 
