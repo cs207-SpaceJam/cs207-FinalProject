a9a61af12d126368e91423dbee3e8ed48f1053db
icweaver <iweaver@cfa.harvard.edu>
2018-12-09 04:00:54 -0500

    fixed typo in setup.py

--- spacejam/setup.py
+++ spacejam/setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
                           'pytest-cov~=2.6.0',
                           'python-dateutil~=2.7.5',
                           'six~=1.11.0'
-        ]
+        ],
         classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
