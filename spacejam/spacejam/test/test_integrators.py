511bc5887f5d3d4ee75d3adeb320e3e4a2964891
Lauren Yoo <jyoo@hsph.harvard.edu>
2018-12-11 01:09:10 -0500

    updated test_integrators.py

--- spacejam/spacejam/test/test_integrators.py
+++ spacejam/spacejam/test/test_integrators.py
@@ -21,15 +21,41 @@ def f(x):
     return np.array([f1])
 
 # analytic solution
-def analytic(t):
+def analytic(t): #t=np.arange(0, T=5, h=0.01)
     x = np.exp(-t)
     return x
+
+# the X_test is the analytical result
 X_test = analytic(t)
 
-# run simulation
+# run simulation for Adams-Moulton order 0
 for n in range(N-1):
     X[n+1] = sj.integrators.amso(f, np.array([X[n]]), h=h, X_tol=1E-4)
 
-tol = 0.0032
-diff = np.abs(X[-1] - X_test[-1])
-assert diff < tol
+tol_0 = 0.0032 
+diff_0 = np.abs(X[-1] - X_test[-1]) #X[-1]==0.009920974201040624, X_test[-1]==0.006805664492230543
+assert diff_0 < tol_0
+
+# run simulation for Adams-Moulton order 1
+X = np.zeros(N); X[0] = 1; t = np.arange(0, T, h)
+for n in range(N-1):
+    X[n+1] = sj.integrators.amsi(f, np.array([X[n]]), h=h, X_tol=1E-4)
+
+tol_1 = 0.0032 
+diff_1 = np.abs(X[-1] - X_test[-1]) 
+assert diff_1 < tol_1
+
+
+# run simulation for Adams-Moulton order 2
+X = np.zeros(N); X[0] = 1; t = np.arange(0, T, h)
+
+for n in range(N-1):
+    X[n+1] = sj.integrators.amsii(f, np.array([X[n]]), np.array([X[n-1]]),h=h, X_tol=1E-4)
+
+tol_2 = 0.00315
+diff_2 = np.abs(X[-1] - X_test[-1]) 
+assert diff_2 < tol_2
+
+
+##ANOTHER TEST##
+##When i > i_tol: 
