3c6d0f5614f0842b0708927b9f16221b4f71148a
Lauren Yoo <jyoo@hsph.harvard.edu>
2018-12-11 20:38:20 -0500

    updating test_integrators.py

--- spacejam/spacejam/test/test_integrators.py
+++ spacejam/spacejam/test/test_integrators.py
@@ -1,13 +1,9 @@
 import spacejam as sj
 import pytest
 import numpy as np
+import sys
 
-# relationship beteen time and step (N*h = T)
-# h: timestep
-# N: total number of steps
-# T: total time
 
-# Initial conditions
 h = 0.01
 N = 500
 T = h*N
@@ -28,34 +24,58 @@ def analytic(t): #t=np.arange(0, T=5, h=0.01)
 # the X_test is the analytical result
 X_test = analytic(t)
 
-# run simulation for Adams-Moulton order 0
-for n in range(N-1):
-    X[n+1] = sj.integrators.amso(f, np.array([X[n]]), h=h, X_tol=1E-4)
+# test simulation for Adams-Moulton order 0 with a toy example
+def test_amso():
+	# Initial conditions
+	
+	for n in range(N-1):
+	    X[n+1] = sj.integrators.amso(f, np.array([X[n]]), h=h, X_tol=1E-4)
 
-tol_0 = 0.0032 
-diff_0 = np.abs(X[-1] - X_test[-1]) #X[-1]==0.009920974201040624, X_test[-1]==0.006805664492230543
-assert diff_0 < tol_0
+	tol_0 = 0.0032 
+	diff_0 = np.abs(X[-1] - X_test[-1]) 
 
-# run simulation for Adams-Moulton order 1
-X = np.zeros(N); X[0] = 1; t = np.arange(0, T, h)
-for n in range(N-1):
-    X[n+1] = sj.integrators.amsi(f, np.array([X[n]]), h=h, X_tol=1E-4)
+	assert diff_0 < tol_0
 
-tol_1 = 0.0032 
-diff_1 = np.abs(X[-1] - X_test[-1]) 
-assert diff_1 < tol_1
+# test simulation for Adams-Moulton order 1 with a toy example
+def test_amsi():
+	X = np.zeros(N); X[0] = 1; t = np.arange(0, T, h)
 
+	for n in range(N-1):
+	    X[n+1] = sj.integrators.amsi(f, np.array([X[n]]), h=h, X_tol=1E-4)
 
-# run simulation for Adams-Moulton order 2
-X = np.zeros(N); X[0] = 1; t = np.arange(0, T, h)
-
-for n in range(N-1):
-    X[n+1] = sj.integrators.amsii(f, np.array([X[n]]), np.array([X[n-1]]),h=h, X_tol=1E-4)
-
-tol_2 = 0.00315
-diff_2 = np.abs(X[-1] - X_test[-1]) 
-assert diff_2 < tol_2
+	tol_1 = 0.0032 
+	diff_1 = np.abs(X[-1] - X_test[-1]) 
+	assert diff_1 < tol_1
 
 
-##ANOTHER TEST##
-##When i > i_tol: 
+# test simulation for Adams-Moulton order 2 with a toy example
+def test_amsii():
+	X = np.zeros(N); X[0] = 1; t = np.arange(0, T, h)
+
+	for n in range(N-1):
+	    X[n+1] = sj.integrators.amsii(f, np.array([X[n]]), np.array([X[n-1]]),h=h, X_tol=1E-4)
+
+	tol_2 = 0.00315
+	diff_2 = np.abs(X[-1] - X_test[-1]) 
+	assert diff_2 < tol_2
+
+
+
+
+# def test_amso_diverge(capsys):
+# 	with self.assertRaises(SystemExit) as cm:
+# 	# with pytest.raises(SystemExit):
+# 		for n in range(N-1):
+# 			X[n+1] = sj.integrators.amso(f, np.array([X[n]]), h=h, X_tol=1E-6, i_tol=0)
+
+# 	self.assertEqual(cm.exception.code, 1)
+# 	# out, err = capsys.readouterr()
+# 	# assert err == 1
+
+#def test_amso_diverge():
+	# with pytest.raises(SystemExit) as pytest_wrapped_e:
+	# 	for n in range(N-1):
+	# 		X[n+1] = sj.integrators.amso(f, np.array([X[n]]), h=h, X_tol=1E-6, i_tol=0)
+	# assert pytest_wrapped_e == SystemExit
+	# assert pytest_wrapped_e.value.code==1
+	
