6824c50ef40f009f2df402c8c8d309ae05e5c7c4
Ian Weaver <iweaver@mailbox.org>
2021-12-30 09:11:37 -0500

    modernizing codebase

--- spacejam/spacejam/dual.py
+++ spacejam/spacejam/dual.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-class Dual():
-    """ Creates dual numbers and defines dual number math.
+
+class Dual:
+    """Creates dual numbers and defines dual number math.
 
     A real number `a` is taken in and its dual counterpart `a + eps [1.00]` is
-    returned to facilitate automatic differentiation in 
+    returned to facilitate automatic differentiation in
     :any:`spacejam.autodiff` .
 
     Notes
@@ -38,38 +39,35 @@ class Dual():
         # set numpy display output to be formatted to two decimal places
         # for easier doctesting and dedicated user dual number creation
         formatter = lambda x: "%.2f" % x
-        np.set_printoptions(formatter={
-                                'int_kind':formatter,
-                                'float_kind':formatter
-                                })
+        np.set_printoptions(formatter={"int_kind": formatter, "float_kind": formatter})
 
         # load func(p) into self.r
         self.r = np.array(real)
 
         # prepare self.d for differentiation
-        if idx is not None: # dual basis vector
+        if idx is not None:  # dual basis vector
             self.d = np.zeros(x.size)
             self.d[idx] = 1.00
-        else: # regular dual vector
+        else:  # regular dual vector
             if dual is not None:
                 self.d = np.array(dual)
             else:
                 self.d = np.ones_like(self.r)
 
     def __add__(self, other):
-        """ Returns the addition of self and other
-        
+        """Returns the addition of self and other
+
         Parameters
         ----------
         self: Dual object
         other: Dual object, float, or int
-        
+
         Returns
-        ------- 
+        -------
         z: Dual object that is the sum of self and other
-        
+
         Examples
-        -------- 
+        --------
         >>> z = Dual(1, 2) + Dual(3, 4)
         >>> print(z)
         4.00 + eps 6.00
@@ -88,29 +86,29 @@ class Dual():
 
         z = Dual(real, dual)
         return z
-    
-    __radd__ = __add__ # addition commutes
-    
+
+    __radd__ = __add__  # addition commutes
+
     def __sub__(self, other):
-        """ Returns the subtraction of self and other
-        
+        """Returns the subtraction of self and other
+
         Parameters
         ----------
         self: Dual object
         other: Dual object, float, or int
-        
+
         Returns
-        ------- 
+        -------
         z: Dual object
            difference of self and other
-           
+
         NOTES
-        ----- 
-        Subtraction does not commute in general. 
+        -----
+        Subtraction does not commute in general.
         A specialized __rsub__ is required.
 
         Examples
-        -------- 
+        --------
         >>> z = Dual(1, 2) - Dual(3, 4)
         >>> print(z)
         -2.00 - eps 2.00
@@ -121,53 +119,53 @@ class Dual():
         try:
             real = self.r - other.r
             dual = self.d - other.d
-        
+
         except AttributeError:
             real = self.r - other
-            dual = self.d 
-        
+            dual = self.d
+
         z = Dual(real, dual)
         return z
-    
+
     def __rsub__(self, other):
-        """ Returns the subtraction of other from self
-        
+        """Returns the subtraction of other from self
+
         Parameters
         ----------
         self: Dual object
         other: Dual object, float, or int
-        
+
         Returns
-        ------- 
+        -------
         z: Dual object
            difference of other and self
 
         Examples
-        -------- 
+        --------
         >>> z = 2 - Dual(1, 2)
         >>> print(z)
         1.00 - eps 2.00
         """
         real = other - self.r
         dual = -self.d
-        
+
         z = Dual(real, dual)
         return z
-    
+
     def __mul__(self, other):
-        """ Returns the product of self and other
-        
+        """Returns the product of self and other
+
         Parameters
         ----------
         self: Dual object
         other: Dual object, float, or int
 
         Returns
-        ------- 
+        -------
         z: Dual object that is the product of self and other
-        
+
         Examples
-        -------- 
+        --------
         >>> z = Dual(1, 2) * Dual(3, 4)
         >>> print(z)
         3.00 + eps 10.00
@@ -176,31 +174,31 @@ class Dual():
         2.00 + eps 4.00
         """
         try:
-            real = self.r*other.r 
-            dual = self.r*other.d + self.d*other.r
+            real = self.r * other.r
+            dual = self.r * other.d + self.d * other.r
         except AttributeError:
-            real = self.r*other
-            dual = self.d*other
-        
+            real = self.r * other
+            dual = self.d * other
+
         z = Dual(real, dual)
         return z
 
-    __rmul__ = __mul__ # multiplication commutes	
-    
+    __rmul__ = __mul__  # multiplication commutes
+
     def __truediv__(self, other):
-        """ Returns the quotient of self and other
-        
+        """Returns the quotient of self and other
+
         Parameters
         ----------
         self: Dual object
         other: Dual object, float, or int
-        
+
         Returns
-        ------- 
+        -------
         z: Dual object that is the quotient of self and other
-        
+
         Examples
-        -------- 
+        --------
         >>> z = Dual(1, 2) / 2
         >>> print(z)
         0.50 + eps 1.00
@@ -209,53 +207,53 @@ class Dual():
         3.00 - eps 2.00
         """
         try:
-            real = (self.r*other.r)/other.r**2
-            dual = (self.d*other.r - self.r*other.d)/other.r**2
+            real = (self.r * other.r) / other.r ** 2
+            dual = (self.d * other.r - self.r * other.d) / other.r ** 2
         except AttributeError:
-            real = (self.r*other)/other**2
-            dual = (self.d*other)/other**2
-        
+            real = (self.r * other) / other ** 2
+            dual = (self.d * other) / other ** 2
+
         z = Dual(real, dual)
         return z
-    
+
     def __rtruediv__(self, other):
-        """ Returns the quotient of other and self
-        
+        """Returns the quotient of other and self
+
         Parameters
         ----------
         self: Dual object
         other: Dual object, float, or int
-        
+
         Returns
-        ------- 
+        -------
         z: Dual object that is the product of self and other
-        
+
         Examples
-        --------  
+        --------
         >>> z = 2 / Dual(1, 2)
         >>> print(z)
         2.00 - eps 4.00
         """
-        real = (other*self.r)/self.r**2
-        dual = -(other*self.d)/self.r**2
-        
+        real = (other * self.r) / self.r ** 2
+        dual = -(other * self.d) / self.r ** 2
+
         z = Dual(real, dual)
         return z
 
     def __pow__(self, other):
-        """ Performs (self.r + eps self.d) ** (other.r + eps other.d)
+        """Performs (self.r + eps self.d) ** (other.r + eps other.d)
 
         Parameters
         ----------
         self: Dual object
         other: Dual object, float, or int
-        
+
         Returns
-        ------- 
+        -------
         z: Dual object that is self raised to the other power
-        
+
         Examples
-        -------- 
+        --------
         >>> z = Dual(1, 2) ** Dual(3, 4)
         >>> print(z)
         1.00 + eps 6.00
@@ -265,28 +263,27 @@ class Dual():
             instance_check = other.r, other.d
         except AttributeError:
             other = Dual(other, 0)
-            
+
         if self.r < 0:
-            raise Exception("The real value should be positive") 
-            
-        real = self.r**other.r
-        dual = self.r**(other.r - 1)*self.d*other.r
+            raise Exception("The real value should be positive")
+
+        real = self.r ** other.r
+        dual = self.r ** (other.r - 1) * self.d * other.r
         if other.d != 0:
-            dual += self.r**other.r*other.d*np.log(self.r)
+            dual += self.r ** other.r * other.d * np.log(self.r)
 
         z = Dual(real, dual)
         return z
-            
+
     def sqrt(self):
-        z = self**(0.5)
+        z = self ** (0.5)
         return z
 
-
     def __pos__(self):
-        """ Returns self
+        """Returns self
 
         Examples
-        -------- 
+        --------
         >>> z = Dual(1, 2)
         >>> print(+z)
         1.00 + eps 2.00
@@ -294,10 +291,10 @@ class Dual():
         return Dual(self.r, self.d)
 
     def __neg__(self):
-        """ Returns negation of self
+        """Returns negation of self
 
         Examples
-        -------- 
+        --------
         >>> z = Dual(1, 2)
         >>> print(-z)
         -1.00 - eps 2.00
@@ -305,97 +302,97 @@ class Dual():
         return Dual(-self.r, -self.d)
 
     def exp(self):
-        """ Returns e**self
-        
+        """Returns e**self
+
         Parameters
         ----------
         self: Dual object
-        
+
         Returns
-        ------- 
+        -------
         z: e**self
-        
+
         Examples
-        -------- 
+        --------
         >>> z = np.exp(Dual(1, 2))
         >>> print(z)
         2.72 + eps 5.44
         """
-        real = np.e**self.r
-        dual = np.e**self.r * self.d
+        real = np.e ** self.r
+        dual = np.e ** self.r * self.d
         return Dual(real, dual)
 
-    #Trigonometric functions that numpy looks for
+    # Trigonometric functions that numpy looks for
     def sin(self):
-        """ Returns the sine of a
-        
+        """Returns the sine of a
+
         Parameters
         ----------
         self: Dual object
-        
+
         Returns
-        ------- 
+        -------
         z: sine of self
-        
+
         Examples
-        -------- 
+        --------
         >>> z = np.sin(Dual(0, 1))
         >>> print(z)
         0.00 + eps 1.00
         """
-        return Dual(np.sin(self.r), self.d*np.cos(self.r))
-        
+        return Dual(np.sin(self.r), self.d * np.cos(self.r))
+
     def cos(self):
-        """ Returns the cosine of a
-        
+        """Returns the cosine of a
+
         Parameters
         ----------
         self: Dual object
-        
+
         Returns
-        ------- 
+        -------
         z: cosine of self
-        
+
         Examples
-        -------- 
+        --------
         >>> z = np.cos(Dual(0, 1))
         >>> print(z)
         1.00 + eps -0.00
         """
-        return Dual(np.cos(self.r), -self.d*np.sin(self.r))
+        return Dual(np.cos(self.r), -self.d * np.sin(self.r))
 
     def tan(self):
-        """ Returns the tangent of a
-        
+        """Returns the tangent of a
+
         Parameters
         ----------
         self: Dual object
-        
+
         Returns
-        ------- 
+        -------
         z: tangent of self
-        
+
         Examples
-        -------- 
+        --------
         >>> z = np.tan(Dual(0,1))
         >>> print(z)
         0.00 + eps 1.00
         """
         z = self.sin() / self.cos()
         return Dual(z.r, z.d)
-        #return Dual(tan(self.r), self.d / np.cos(self.r)**2)
-        
+        # return Dual(tan(self.r), self.d / np.cos(self.r)**2)
+
     def __repr__(self):
-        """ Prints self in the form a_r + eps a_d, where self = Dual(a_r, a_d),
+        """Prints self in the form a_r + eps a_d, where self = Dual(a_r, a_d),
         a_r and a_d are the real and dual part of self, respectively,
         and both terms are automatically rounded to two decimal places
-        
+
         Returns
-        ------- 
+        -------
         z: Dual object that is the product of self and other
-        
+
         Examples
-        -------- 
+        --------
         >>> z = Dual(1, 2)
         >>> print(z)
         1.00 + eps 2.00
@@ -403,22 +400,19 @@ class Dual():
         # set numpy display output to be formatted to two decimal places
         # for easier doctesting
         float_formatter = lambda x: "%.2f" % x
-        np.set_printoptions(formatter={
-                                'int_kind':float_formatter,
-                                'float_kind':float_formatter
-                                })
+        np.set_printoptions(formatter={"int_kind": float_formatter, "float_kind": float_formatter})
 
         # format dual numbers accordingly if p is scalar or vector
         if self.r.size > 1:
-            s = f'{self.r} + eps {self.d}' 
+            s = f"{self.r} + eps {self.d}"
             return s
 
         if self.d.size == 1:
             if self.d >= 0:
-                s = f'{self.r:.2f} + eps {self.d:.2f}' 
+                s = f"{self.r:.2f} + eps {self.d:.2f}"
             else:
-                s = f'{self.r:.2f} - eps {np.abs(self.d):.2f}'
+                s = f"{self.r:.2f} - eps {np.abs(self.d):.2f}"
         else:
-            s = f'{self.r:.2f} + eps {self.d}' 
+            s = f"{self.r:.2f} + eps {self.d}"
 
         return s
--- spacejam/spacejam/integrators.py
+++ spacejam/spacejam/integrators.py
@@ -7,8 +7,9 @@ Adams-Moulton orders (i.e. s = 0, 1, 2).
 See: https://en.wikipedia.org/wiki/Linear_multistep_method#Adams%E2%80%93Moulton_methods
 """
 
-def amso(func, X_old, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
-    """ Zeroth order Adams-Moulton method (AKA Backward Euler)
+
+def amso(func, X_old, h=1e-3, X_tol=1e-1, i_tol=1e2, kwargs=None):
+    """Zeroth order Adams-Moulton method (AKA Backward Euler)
 
     Parameters
     ----------
@@ -24,7 +25,7 @@ def amso(func, X_old, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
             Maximum number of Newton-Raphson iterations. Entire simulation
             terminates if this number is exceeded.
     kwargs : dict (default None)
-             optional arguments to be supplied to user defined function. 
+             optional arguments to be supplied to user defined function.
 
     Returns
     -------
@@ -33,16 +34,16 @@ def amso(func, X_old, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
 
     Examples
     --------
-    >>> 
-    """ 
+    >>>
+    """
     if kwargs:
         ad = autodiff.AutoDiff(func, X_old, kwargs=kwargs)
     else:
         ad = autodiff.AutoDiff(func, X_old)
-    
+
     # Initial guess with forward Euler
-    X_new = X_old + h*ad.r.flatten() # X_{n+1}^(0)
-    
+    X_new = X_old + h * ad.r.flatten()  # X_{n+1}^(0)
+
     # Iterate to better solution for X_new using Newton-Raphson method
     X_iold = X_old
     X_inew = X_new
@@ -51,14 +52,16 @@ def amso(func, X_old, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
         X_iold = X_inew
 
         if i > i_tol:
-            msg = "\nSorry, spacejam did not converge for s=0 A-M method.\n" \
-                  "Try adjusting X_tol, i_tol, or using another integrator."
+            msg = (
+                "\nSorry, spacejam did not converge for s=0 A-M method.\n"
+                "Try adjusting X_tol, i_tol, or using another integrator."
+            )
             sys.exit(msg)
 
-        ad = autodiff.AutoDiff(func, X_iold, kwargs=kwargs) # get Jacobian (ad.d)
+        ad = autodiff.AutoDiff(func, X_iold, kwargs=kwargs)  # get Jacobian (ad.d)
         I = np.eye(len(ad.r.flatten()))
-        D = I - h*ad.d
-        g = X_iold - X_old - h*ad.r.flatten()
+        D = I - h * ad.d
+        g = X_iold - X_old - h * ad.r.flatten()
 
         X_inew = X_iold - np.dot(np.linalg.pinv(D), g)
 
@@ -69,8 +72,9 @@ def amso(func, X_old, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
     X_new = X_iold
     return X_new
 
-def amsi(func, X_old, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
-    """ First order Adams-Moulton method (AKA Trapezoid)
+
+def amsi(func, X_old, h=1e-3, X_tol=1e-1, i_tol=1e2, kwargs=None):
+    """First order Adams-Moulton method (AKA Trapezoid)
 
     Parameters
     ----------
@@ -86,7 +90,7 @@ def amsi(func, X_old, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
             Maximum number of Newton-Raphson iterations. Entire simulation
             terminates if this number is exceeded.
     kwargs : dict (default None)
-             optional arguments to be supplied to user defined function. 
+             optional arguments to be supplied to user defined function.
 
     Returns
     -------
@@ -97,10 +101,10 @@ def amsi(func, X_old, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
         ad = autodiff.AutoDiff(func, X_old, kwargs=kwargs)
     else:
         ad = autodiff.AutoDiff(func, X_old)
-    
+
     # Initial guess with forward Euler
-    X_new = X_old + h*ad.r.flatten() # X_{n+1}^(0)
-    
+    X_new = X_old + h * ad.r.flatten()  # X_{n+1}^(0)
+
     # Iterate to better solution for X_new using Newton-Raphson method
     X_iold = X_old
     X_inew = X_new
@@ -109,16 +113,17 @@ def amsi(func, X_old, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
         X_iold = X_inew
 
         if i > i_tol:
-            msg = "\nSorry, spacejam did not converge for s=1 A-M method.\n" \
-                  "Try adjusting X_tol, i_tol, or using another integrator."
+            msg = (
+                "\nSorry, spacejam did not converge for s=1 A-M method.\n"
+                "Try adjusting X_tol, i_tol, or using another integrator."
+            )
             sys.exit(msg)
-             
 
-        ad = autodiff.AutoDiff(func, X_iold, kwargs=kwargs) # get Jacobian (ad.d)
-        ad_n = autodiff.AutoDiff(func, X_old, kwargs=kwargs) # for X_n
+        ad = autodiff.AutoDiff(func, X_iold, kwargs=kwargs)  # get Jacobian (ad.d)
+        ad_n = autodiff.AutoDiff(func, X_old, kwargs=kwargs)  # for X_n
         I = np.eye(len(ad.r.flatten()))
-        D = I - (h/2)*ad.d
-        g = X_iold - X_old - (h/2)*ad.r.flatten() - (h/2)*ad_n.r.flatten()
+        D = I - (h / 2) * ad.d
+        g = X_iold - X_old - (h / 2) * ad.r.flatten() - (h / 2) * ad_n.r.flatten()
 
         X_inew = X_iold - np.dot(np.linalg.pinv(D), g)
 
@@ -129,8 +134,9 @@ def amsi(func, X_old, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
     X_new = X_iold
     return X_new
 
-def amsii(func, X_n, X_nn, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
-    """ Second order Adams-Moulton method
+
+def amsii(func, X_n, X_nn, h=1e-3, X_tol=1e-1, i_tol=1e2, kwargs=None):
+    """Second order Adams-Moulton method
 
     Parameters
     ----------
@@ -148,7 +154,7 @@ def amsii(func, X_n, X_nn, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
             Maximum number of Newton-Raphson iterations. Entire simulation
             terminates if this number is exceeded.
     kwargs : dict (default None)
-             optional arguments to be supplied to user defined function. 
+             optional arguments to be supplied to user defined function.
 
     Returns
     -------
@@ -159,10 +165,10 @@ def amsii(func, X_n, X_nn, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
         ad = autodiff.AutoDiff(func, X_n, kwargs=kwargs)
     else:
         ad = autodiff.AutoDiff(func, X_n)
-    
+
     # Initial guess with forward Euler
-    X_new = X_n + h*ad.r.flatten() # X_{n+1}^(0)
-    
+    X_new = X_n + h * ad.r.flatten()  # X_{n+1}^(0)
+
     # Iterate to better solution for X_new using Newton-Raphson method
     X_iold = X_n
     X_inew = X_new
@@ -171,21 +177,23 @@ def amsii(func, X_n, X_nn, h=1E-3, X_tol=1E-1, i_tol=1E2, kwargs=None):
         X_iold = X_inew
 
         if i > i_tol:
-            msg = "\nSorry, spacejam did not converge for s=2 A-M method.\n" \
-                  "Try adjusting X_tol, i_tol, or using another integrator."
+            msg = (
+                "\nSorry, spacejam did not converge for s=2 A-M method.\n"
+                "Try adjusting X_tol, i_tol, or using another integrator."
+            )
             sys.exit(msg)
 
-        ad = autodiff.AutoDiff(func, X_iold, kwargs=kwargs) # get Jacobian (ad.d)
-        ad_n = autodiff.AutoDiff(func, X_n, kwargs=kwargs) # X_n
-        ad_nn = autodiff.AutoDiff(func, X_nn, kwargs=kwargs) # X_n-1
+        ad = autodiff.AutoDiff(func, X_iold, kwargs=kwargs)  # get Jacobian (ad.d)
+        ad_n = autodiff.AutoDiff(func, X_n, kwargs=kwargs)  # X_n
+        ad_nn = autodiff.AutoDiff(func, X_nn, kwargs=kwargs)  # X_n-1
         I = np.eye(len(ad.r.flatten()))
-        D = I - (5*h/12)*ad.d
+        D = I - (5 * h / 12) * ad.d
         g1 = X_iold
         g2 = X_n
-        g3 = (5/12)*ad.r.flatten()
-        g4 = (2/3)*ad_n.r.flatten()
-        g5 = (1/12)*ad_nn.r.flatten()
-        g = g1 - g2 - h*(g3 + g4 - g5)
+        g3 = (5 / 12) * ad.r.flatten()
+        g4 = (2 / 3) * ad_n.r.flatten()
+        g5 = (1 / 12) * ad_nn.r.flatten()
+        g = g1 - g2 - h * (g3 + g4 - g5)
         X_inew = X_iold - np.dot(np.linalg.pinv(D), g)
 
         # update
